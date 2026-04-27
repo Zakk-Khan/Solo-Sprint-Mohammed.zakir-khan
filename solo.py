7b986e55f286744f1da086fcd855234c8b14d09b
[Mohammed Zakir Khan] <[zakir.khan124@outlook.com]>
2026-04-27 11:01:45 +0100

    solo-project

--- solo.py
+++ solo.py
@@ -1,3 +1,16 @@
-class add 
-    def_inital_comment
+active_users = []
+disabled_users = []
+
+def display_menu():
+    print("1. Add User")
+    print("2. View active/disabled Users")
+    print("3. Enable/Disable Users")
+    print("0. Exit")
+
+if display_menu == 1:
+    print("Input Username")
+elif display_menu == 2:
+    print(active_users + disabled_users)
+elif display_menu == 3:
     
+
